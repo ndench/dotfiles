04bc5c2619242c29ee89669629a32c9597c3cc36
Nathan Dench <ndenc2@gmail.com>
2019-09-25 17:37:12 +1000

    Copy screenshots to clipboard

--- tag-new-dotfiles/zsh/settings/aliases.zsh
+++ tag-new-dotfiles/zsh/settings/aliases.zsh
@@ -73,4 +73,8 @@ alias cprov='cd ~/git/ndench/provision-local'
 alias eprov='vim ~/git/ndench/provision-local'
 alias mprov='make -C ~/git/ndench/provision-local'
 
+# Custom aliases
 alias tf='terraform'
+alias xclip='xclip -sel clip'
+alias xsel='xsel -b'
+alias ctmp='cd $(mktemp -d)'
