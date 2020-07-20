d2a8a6fded1ec79f5ac198ce8d9ba3fe1c7e28bb
Nathan Dench <ndenc2@gmail.com>
2020-07-20 15:43:11 +1000

    sort out new desktop monitor scripts

--- tag-new-dotfiles/screenlayout/desktop.sh
+++ tag-new-dotfiles/screenlayout/desktop.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1920x1080 --pos 2560x360 --rotate normal --output DP1 --off --output DP2 --off --output DP2-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2-2 --off --output DP2-3 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${DIR}/desktop1.sh
+source ${DIR}/desktop2.sh
